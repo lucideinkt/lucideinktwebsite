8913b73de8f06f40375913890b376c77fb73495e
Bilal van Loon <vanloon_1983@hotmail.com>
2025-12-06 17:09:07 +0100

    Added Title Logic Improvement

--- resources/views/components/dashboard-layout.blade.php
+++ resources/views/components/dashboard-layout.blade.php
@@ -3,11 +3,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-    @if(app()->environment('staging'))
-        Staging Lucide Inkt | Dashboard
-    @else
-        Lucide Inkt | Dashboard
-    @endif
+    <title>
+        @if(app()->environment('staging'))
+            Staging Lucide Inkt | Dashboard
+        @else
+            Lucide Inkt | Dashboard
+        @endif
+    </title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
   <script src="https://unpkg.com/leaflet/dist/leaflet.js"></script>
