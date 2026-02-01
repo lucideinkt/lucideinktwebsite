9e580c6e6a1b6dd3011ffa3e7d4d38523da9981b
Bilal van Loon <vanloon_1983@hotmail.com>
2026-02-01 20:34:21 +0100

    Added Said Nursi Page

--- resources/views/saidnursi.blade.php
+++ resources/views/saidnursi.blade.php
@@ -2,10 +2,17 @@
     <main class="container page">
         <div class="said-nursi-page__text-box">
             <h1 class="title">Wie is Said Nursî?</h1>
-            <h2 class="sub-title">1878 – 1960</h2>
+
+            <div class="top-images">
+            <img src="{{ asset('images/Rose_Said.webp') }}" alt="">
+            <img src="{{ asset('images/young_said_nursi.webp') }}" alt="">
+            <img src="{{ asset('images/oval_ornament.webp') }}" alt="">
+            </div>
+
+            <h2 class="sub-title"><em>1878 – 1960</em></h2>
 
             <blockquote>
-                <p>"Ik zal de wereld bewijzen dat de Qur'an een spirituele Zon is Die nimmer zal doven en door niemand kan worden uitgedoofd!"</p>
+                <p><strong>"Ik zal de wereld bewijzen dat de Qur'an een spirituele Zon is Die nimmer zal doven en door niemand kan worden uitgedoofd!"</strong></p>
             </blockquote>
 
             <p>Dit was de reactie van Said Nursî toen hij in zijn vroege twintiger jaren de volgende tekst in een krant las:</p>
@@ -16,32 +23,34 @@
 
             <p>Dit duistere bericht ontstak een brandend vuur in de jonge Said. Met een onwankelbare vastberadenheid stippelde hij zijn levenspad uit. Zonder ooit op zijn besluit terug te komen, zou hij de rest van zijn leven volledig wijden aan de dienst van de Qur'an. De ultieme vrucht van deze onverzettelijke keuze rijpte geleidelijk uit tot de Risale-i Nur.</p>
 
-            <h3>Jeugd en opleiding</h3>
             <p>In een afgelegen dorp in Oost-Anatolië genaamd Nurs was hij ter wereld gekomen. Zijn vader Mirza was een Godvrezende veehouder; zijn moeder Nuriye een vrome dorpsvrouw. Terwijl zijn vader hun dieren altijd muilkorfde wanneer hij ze naar hun graasweide bracht – zodat ze onderweg geen hap van andermans gewas namen – gaf zijn moeder hem altijd alleen borstvoeding nadat zij de rituele wassing had verricht. Zo genoot Said Nursî een uiterst zuivere opvoeding die bijdroeg aan de gezegende ontplooiing van zijn buitengewone gaven.</p>
 
-            <p>Van kinds af aan dorstte hij naar kennis. Dit dreef hem ertoe om op negenjarige leeftijd zijn ouderhuis te verlaten. In de studiejaren die volgden, verbleef hij in verscheidene medresses. Een studie die normaliter vijftien jaar duurt, rondde hij in drie maanden af. Voordat hij de pubertijd bereikte, ontving hij op veertienjarige leeftijd zijn "idjazah" (onderwijsbevoegdheid). Geleerden die getuigen waren van zijn onmiskenbare begaafdheid gaven hem de eretitel "Bediüzzaman" (de onvergelijkelijke des tijds).</p>
+            <img src="{{ asset('images/said_nursi_standing.webp') }}" alt="">
 
-            <h3>De Goddelijke beschikking</h3>
-            <p>Op een dag had hij de profeet Mohammed (s.a.w) in zijn droom gezien en hem om kennis verzocht. Daarop gaf de profeet hem de tijding dat hem Qur'anische kennis geschonken zou worden, op voorwaarde dat hij geen vragen aan de oemma zou stellen. Deze tijding werd tijdens zijn leven verwezenlijkt. Op jeugdige leeftijd werd hij bekend als een uitblinkende geleerde van zijn tijdperk. Nooit stelde hij vragen aan anderen, terwijl hij altijd op alle vragen die hem gesteld werden heldere antwoorden gaf. Naarmate de tijd verstreek, bleek dat hij door de Goddelijke Beschikking was uitverkozen om een belangrijke taak te vervullen.</p>
+            <p>Van kinds af aan dorstte hij naar kennis. Dit dreef hem ertoe om op negenjarige leeftijd zijn ouderhuis te verlaten. In de studiejaren die volgden, verbleef hij in verscheidene medresses. Een studie die normaliter vijftien jaar duurt, rondde hij in drie maanden af. Voordat hij de pubertijd bereikte, ontving hij op veertienjarige leeftijd zijn "<strong>idjazah</strong>" (onderwijsbevoegdheid). Geleerden die getuigen waren van zijn onmiskenbare begaafdheid gaven hem de eretitel "<strong>Bediüzzaman</strong>" (de onvergelijkelijke des tijds).</p>
 
-            <h3>De Mudjeddid</h3>
-            <p>Zoals in een Hadith is overgeleverd, heeft de profeet Mohammed (s.a.w) het volgende verkondigd: "Aan het begin van elke eeuw zal ALLAH een "Mudjeddid" (vernieuwer) zenden." Deze "Mudjeddid" zal vertroebelde waarheden herstellen. Elke Mudjeddid is een uitverkoren geleerde die in zijn tijd de Qur'an en de Sunnah op de meest correcte wijze interpreteert en op de beste wijze verklaart. Zonder enige verandering in de Islamitische basisprincipes of kerngedachtes aan te brengen, hanteert een Mudjeddid nieuwe beschrijvingsmethodes en redeneringen om alle problemen en vraagstukken van de heersende tijdgeest te ontwarren en op te lossen.</p>
+            <p>Op een dag had hij de profeet Mohammed <strong>ﷺ</strong> in zijn droom gezien en hem om kennis verzocht. Daarop gaf de profeet hem de tijding dat hem Qur'anische kennis geschonken zou worden, op voorwaarde dat hij geen vragen aan de oemma zou stellen. Deze tijding werd tijdens zijn leven verwezenlijkt. Op jeugdige leeftijd werd hij bekend als een uitblinkende geleerde van zijn tijdperk. Nooit stelde hij vragen aan anderen, terwijl hij altijd op alle vragen die hem gesteld werden heldere antwoorden gaf. Naarmate de tijd verstreek, bleek dat hij door de Goddelijke Beschikking was uitverkozen om een belangrijke taak te vervullen.</p>
 
-            <p>Omdat de tijdgenoten van Said Nursî zonneklare herkenningstekenen van een Mudjeddid bij hem zagen, noemden zij hem "de Mudjeddid van het einde der tijden". In de Risale-i Nur geeft Said Nursî echter aan dat hem de titel "Mudjeddid" tijdelijk is toegekend om die uiteindelijk over te dragen aan de Risale-i Nur.</p>
+            <p>Zoals in een Hadith is overgeleverd, heeft de profeet Mohammed <strong>ﷺ</strong> het volgende verkondigd: "Aan het begin van elke eeuw zal ALLAH een "<strong>Mudjeddid</strong>" (vernieuwer) zenden." Deze "<strong>Mudjeddid</strong>" zal vertroebelde waarheden herstellen. Elke Mudjeddid is een uitverkoren geleerde die in zijn tijd de Qur'an en de Sunnah op de meest correcte wijze interpreteert en op de beste wijze verklaart. Zonder enige verandering in de Islamitische basisprincipes of kerngedachtes aan te brengen, hanteert een Mudjeddid nieuwe beschrijvingsmethodes en redeneringen om alle problemen en vraagstukken van de heersende tijdgeest te ontwarren en op te lossen.</p>
 
-            <h3>Zijn levenswerk</h3>
+            <p>Omdat de tijdgenoten van Said Nursî zonneklare herkenningstekenen van een Mudjeddid bij hem zagen, noemden zij hem "<strong>de Mudjeddid van het einde der tijden</strong>". In de Risale-i Nur geeft Said Nursî echter aan dat hem de titel "Mudjeddid" tijdelijk is toegekend om die uiteindelijk over te dragen aan de Risale-i Nur.</p>
+
             <p>Zijn gehele drieëntachtigjarige bestaan op aarde had hij gewijd aan de geloofsdienst. Ongeacht waar hij zich bevond – in een medresse, op het slagveld, in krijgsgevangenschap, in ballingschap of in een staatsgevangenis – niets kon hem ervan weerhouden om met al zijn kracht te ijveren voor de Islam. Uiteindelijk heeft hij bij Gods Gratie zijn levenswerk kunnen vervolmaken.</p>
 
-            <h3>Uit zijn biografie</h3>
-            <p>Ter afsluiting volgt nu een tekst van Said Nursî zelf uit zijn biografie:</p>
+            <p><strong>Ter afsluiting volgt nu een tekst van Said Nursî zelf uit zijn biografie:</strong></p>
+
+            <img src="{{ asset('images/said_nursi_wijzend.webp') }}" alt="">
 
             <p>Denken zij daadwerkelijk dat ik een zelfzuchtig persoon ben die er slechts op uit is om zijn eigen nefs te redden? Om het geloof van de gemeenschap te redden, heb ik zowel mijn wereld als mijn hiernamaals opgeofferd. Gedurende heel mijn ruim tachtigjarige leven heb ik geen aards genot gekend. Mijn hele leven heb ik op slagvelden, in krijgsgevangenschap of in staatsgevangenissen doorgebracht. Er is geen pijn die ik niet heb geproefd, geen kwelling die ik niet heb ondergaan. Voor krijgsraden ben ik als een woeste misdadiger behandeld; als een verstotene ben ik van oord naar oord verbannen. In staatsgevangenissen werd mij maandenlang elke vorm van contact ontzegd. Ik ben herhaaldelijk vergiftigd en heb velerlei vernederingen moeten verduren. Er zijn tijden geweest waarin ik de dood duizendmaal boven het leven verkoos. Als mijn religie mij niet van zelfmoord had weerhouden, zou Said vandaag waarschijnlijk onder de grond vergaan zijn.</p>
+
 
             <p>Mijn aard verdraagt geen schimp en smaad. De waardigheid en onversaagdheid van de Islam staan mij niet toe om daarvoor te zwichten. Mocht ik er toch mee geconfronteerd worden, dan maakt het niet uit wie tegenover mij staat – hetzij de wreedste despoot, hetzij de bloeddorstigste commandant – ik zal mij niet voor hem vernederen; ik zal hem zijn wreedheid en bloeddorstigheid recht in het gezicht werpen. Of hij mij vervolgens in de kerker gooit of tot de galg veroordeelt, is totaal niet van belang. Zulke omstandigheden hebben zich overigens al voorgedaan; ik heb dit alles al meegemaakt. Hadden het hart en geweten van een wrede commandant nog enkele minuten zijn onrecht getolereerd, dan zou Said als gehangene vandaag in de schare der onschuldigen zijn opgenomen.</p>
 
             <p>Voorwaar, in een aaneenschakeling van beproevingen en rampen is heel mijn leven zwaar en bitter verlopen. Op het pad ten dienste van het geloof, de gelukzaligheid en de vrede van de gemeenschap heb ik mijn nefs en mijn wereld opgeofferd – al mijn geschonden rechten verklaar ik halal. Zelfs de schenders bid ik niets kwaads toe. Want langs deze weg is de Risale-i Nur een middel geworden dat inmiddels het geloof van minstens honderdduizenden, of naar zeggen zelfs miljoenen mensen heeft gered – het exacte aantal weet ik niet, maar de officier van justitie in Afyon had het over vijfhonderdduizend, en wellicht is het nog meer. Met de dood had ik alleen mezelf kunnen redden. Echter, door in leven te blijven en alle moeiten en beproevingen te verdragen, heb ik dienst mogen verrichten om het geloof van zovelen te redden. ALLAH zij duizendmaal geprezen.</p>
 
             <p>Om het geloof van de gemeenschap in veiligheid te brengen, heb ik op dat pad ook mijn hiernamaals opgeofferd. In mijn ogen bestaat er noch liefde voor het paradijs, noch vrees voor de hel. Namens het geloof van niet alleen de Turkse gemeenschap van 25 miljoen, maar van de gehele Islamitische gemeenschap van honderden miljoenen mensen, mogen niet één, maar duizend Saids opgeofferd worden. Als onze Qur'an op aarde zonder volgers achterblijft, dan verlang ik ook niet naar het paradijs; ook daar zal het dan een kerker voor mij zijn. Als ik zou zien dat het geloof van mijn volk veiliggesteld is, dan ben ik bereid om in de vlammen van de hel te branden. Want terwijl mijn lichaam dan knettert in het vuur, zal mijn hart veranderen in een weelderige rozentuin.</p>
+
+            <img src="{{ asset('images/rose_end.webp') }}" alt="">
         </div>
     </main>
     <div class="gradient-border"></div>
