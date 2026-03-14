617d2d644cb3633c100394a402907805ec8123b3
Bilal van Loon <vanloon_1983@hotmail.com>
2026-03-14 16:20:16 +0100

    Add styling

--- resources/views/components/breadcrumbs.blade.php
+++ resources/views/components/breadcrumbs.blade.php
@@ -11,18 +11,24 @@
             @php $isLast = $i === $count - 1; @endphp
 
             <li class="bc-item" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
-                @if($i === 0)
-                    <svg aria-hidden="true" class="bc-home" viewBox="0 0 20 20" fill="currentColor">
-                        <path d="M10.707 1.293a1 1 0 0 0-1.414 0L2 8.586V17a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1v-4h4v4a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1V8.586l-7.293-7.293z"/>
-                    </svg>
-                @endif
-
                 @if(!empty($item['url']) && !$isLast)
                     <a href="{{ $item['url'] }}" class="bc-link" itemprop="item">
+                        @if($i === 0)
+                            <svg aria-hidden="true" class="bc-home" viewBox="0 0 20 20" fill="currentColor">
+                                <path d="M10.707 1.293a1 1 0 0 0-1.414 0L2 8.586V17a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1v-4h4v4a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1V8.586l-7.293-7.293z"/>
+                            </svg>
+                        @endif
                         <span itemprop="name">{{ $item['label'] }}</span>
                     </a>
                 @else
-                    <span class="bc-current" itemprop="name" aria-current="page">{{ $item['label'] }}</span>
+                    <span class="bc-current" itemprop="name" aria-current="page">
+                        @if($i === 0)
+                            <svg aria-hidden="true" class="bc-home" viewBox="0 0 20 20" fill="currentColor">
+                                <path d="M10.707 1.293a1 1 0 0 0-1.414 0L2 8.586V17a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1v-4h4v4a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1V8.586l-7.293-7.293z"/>
+                            </svg>
+                        @endif
+                        {{ $item['label'] }}
+                    </span>
                 @endif
 
                 <meta itemprop="position" content="{{ $i + 1 }}" />
