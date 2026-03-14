c27bf6410e7033f74aae6f6121e46f9271ac2126
Bilal van Loon <vanloon_1983@hotmail.com>
2026-03-14 23:13:47 +0100

    Changed breadcrumb style

--- resources/views/components/breadcrumbs.blade.php
+++ resources/views/components/breadcrumbs.blade.php
@@ -11,22 +11,25 @@
             @php $isLast = $i === $count - 1; @endphp
 
             <li class="bc-item" itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+                @if($i === 0)
+{{--                    <svg aria-hidden="true" class="bc-home" viewBox="0 0 20 20" fill="currentColor">--}}
+{{--                        <path d="M10.707 1.293a1 1 0 0 0-1.414 0L2 8.586V17a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1v-4h4v4a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1V8.586l-7.293-7.293z"/>--}}
+{{--                    </svg>--}}
+                @endif
+
                 @if(!empty($item['url']) && !$isLast)
-                    <a href="{{ $item['url'] }}" class="bc-pill bc-pill--link" itemprop="item">
-                        @if($i === 0)<i class="fa-solid fa-house bc-pill-icon" aria-hidden="true"></i>@endif
+                    <a href="{{ $item['url'] }}" class="bc-link" itemprop="item">
                         <span itemprop="name">{{ $item['label'] }}</span>
                     </a>
                 @else
-                    <span class="bc-pill bc-pill--current" itemprop="name" aria-current="page">
-                        @if($i === 0)<i class="fa-solid fa-house bc-pill-icon" aria-hidden="true"></i>@endif
-                        {{ $item['label'] }}
-                    </span>
+                    <span class="bc-current" itemprop="name" aria-current="page">{{ $item['label'] }}</span>
                 @endif
+
                 <meta itemprop="position" content="{{ $i + 1 }}" />
             </li>
 
             @if(!$isLast)
-                <li class="bc-sep" aria-hidden="true">/</li>
+                <span class="bc-sep" aria-hidden="true">&#8250;</span>
             @endif
         @endforeach
     </ol>
