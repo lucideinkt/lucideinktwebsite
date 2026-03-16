b77ffd464ecbd4697def14e411d8f2ff0ae609a3
Bilal van Loon <vanloon_1983@hotmail.com>
2026-03-16 19:47:03 +0100

    Added new bg hero

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 <x-layout :seo-data="$SEOData">
     <main class="page home">
 
-        <div class="hero-bg-wrapper" style="background-image: url('{{ asset('images/new_banner_hero_5.webp') }}');">
+        <div class="hero-bg-wrapper" style="background-image: url('{{ asset('images/new_banner_hero_6.webp') }}');">
 
             <section class="home-hero">
                 <div class="clock-image">
