1c433bb23b89279fcb0d9d1955501fdf774602f5
Bilal van Loon <bilalvanloon@MacBook-Pro-van-Bilal.local>
2026-05-26 16:28:40 +0200

    Added styles

--- resources/views/newsletter/confirm.blade.php
+++ resources/views/newsletter/confirm.blade.php
@@ -1,42 +1,59 @@
 <x-layout>
-    <main class="container page">
-        <div class="newsletter-unsubscribe-page__text-box">
-            <div class="unsubscribe-icon-wrapper">
-                @if($confirmed)
-                    <div class="confirm-icon-circle confirm-icon-circle--success">
-                        <i class="fa-solid fa-circle-check"></i>
-                    </div>
-                @else
-                    <div class="confirm-icon-circle confirm-icon-circle--info">
-                        <i class="fa-solid fa-envelope-open-text"></i>
-                    </div>
-                @endif
-            </div>
+    <div class="page-normal-background">
+    <main class="container page cart-page">
 
-            <h1 class="title">
-                @if($confirmed)
-                    Inschrijving bevestigd!
-                @else
-                    Al bevestigd
-                @endif
-            </h1>
-
-            <p class="unsubscribe-message">
-                @if($confirmed)
-                    Je inschrijving voor de nieuwsbrief van Lucide Inkt is succesvol bevestigd.
-                    Je ontvangt inshaa'ALLAH onze nieuwsbrief en updates.
-                @else
-                    Je inschrijving was al eerder bevestigd. Je staat al op onze nieuwsbrief.
-                @endif
-            </p>
-
-            <div class="unsubscribe-actions">
-                <a href="{{ route('home') }}" class="btn btn-primary btn-sm">
-                    <i class="fa-solid fa-home"></i> Terug naar homepagina
-                </a>
+        <div class="cart-hero">
+            <div class="container">
+                <x-breadcrumbs :items="[
+                  ['label' => 'Home', 'url' => route('home')],
+                  ['label' => $confirmed ? 'Inschrijving bevestigd' : 'Al bevestigd'],
+                ]" />
             </div>
         </div>
+
+        <div class="gradient-border cart-hero-border"></div>
+
+        <div class="cart-content-section">
+            <div class="cart-empty-state-wrapper">
+                <div class="cart-empty-state">
+                    <div class="empty-state-icon">
+                        @if($confirmed)
+                            <i class="fa-solid fa-circle-check" style="color: #2c582f;"></i>
+                        @else
+                            <i class="fa-solid fa-envelope-open-text"></i>
+                        @endif
+                    </div>
+
+                    <h3>
+                        @if($confirmed)
+                            Inschrijving bevestigd!
+                        @else
+                            Al bevestigd
+                        @endif
+                    </h3>
+
+                    <p>
+                        @if($confirmed)
+                            Je inschrijving voor de nieuwsbrief van Lucide Inkt is succesvol bevestigd.
+                            Je ontvangt inshaa'ALLAH onze nieuwsbrief en updates.
+                        @else
+                            Je inschrijving was al eerder bevestigd. Je staat al op onze nieuwsbrief.
+                        @endif
+                    </p>
+
+                    <div class="empty-state-actions">
+                        <a href="{{ route('home') }}" class="btn-shop">
+                            <i class="fa-solid fa-home"></i>
+                            Terug naar homepagina
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </main>
+
     <div class="gradient-border"></div>
     <x-footer></x-footer>
+    </div>
 </x-layout>
