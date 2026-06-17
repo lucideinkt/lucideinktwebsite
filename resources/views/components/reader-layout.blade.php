71d2a7401da725e046812a92d57cc5abb7152377
Bilal van Loon <bilalvanloon@MacBook-Pro-van-Bilal.local>
2026-06-17 22:38:49 +0200

    Added new breadcrumb

--- resources/views/components/reader-layout.blade.php
+++ resources/views/components/reader-layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="google" content="notranslate">
 
-    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+    <link rel="icon" type="image/png" href="{{ asset('images/google_logo.png') }}">
 
     @if(isset($seoData))
         {!! seo($seoData) !!}
